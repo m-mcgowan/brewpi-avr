4e036e5da5e7beec5c61919b22cc5c0616099590
m-mcgowan <mat.mcgowan+github+profile@gmail.com>
2014-02-10 15:37:10 +0100

    onewire enumeration value on OneWireBus

--- brewpi_avr/BrewpiObjects.h
+++ brewpi_avr/BrewpiObjects.h
@@ -14,7 +14,7 @@
  */
 // todo - I contemplated exposing e.g. pin as a value, but this is immutable, and is available from the object construction arguments
 // mdma: I didn't push the Object functionality down into OneWire because that code comes from an external source - less intrusive to subclass.
-class OneWireBus : public Object, public OneWire
+class OneWireBus : public Value, public OneWire
 {
 	
 public:	
@@ -24,4 +24,26 @@ public:
 		uint8_t pin = def.in->next();
 		return new_object(OneWireBus(pin));
 	}
+	
+	/**
+	 * Enumerates the one-wire bus and returns the devices found.
+	 */
+	void readTo(DataOut& out) {
+		reset_search();
+		uint8_t address[8];
+		while (search(address)) {
+			// hardware device type from OneWire family ID
+			out.write(address, 8);
+		}
+		out.write(0);
+	}
+	
+	uint8_t streamSize() {
+		// todo - what do to when there are more than 32 devices on the bus?
+		// todo - don't really want to have to determine the stream size by enumerating devices to find out how
+		// many there are. Make 255 mean unknown size, and that the caller has to use a device-specific enumeration.		
+		return 255;
+	}
+	
 };
+
