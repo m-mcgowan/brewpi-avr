d277bb6910d2eaaedd1ddec4b5b07723c2e78610
m-mcgowan <mat.mcgowan+github+profile@gmail.com>
2014-03-24 23:50:25 +0100

    grr..unsaved change in netbeans.

--- brewpi_cpp/timems.h
+++ brewpi_cpp/timems.h
@@ -2,26 +2,5 @@
 
 typedef int64_t msec_t;
 
-#if defined(__WIN32__)
-
-#include <windows.h>
-
-inline msec_t time_ms(void)
-{
-    return GetTickCount();
-}
-
-#else
-
-#include <sys/time.h>
-
-msec_t time_ms(void)
-{
-    struct timeval tv;
-    gettimeofday(&tv, 0);
-    return (msec_t)tv.tv_sec * 1000 + tv.tv_usec / 1000;
-}
-
-#endif
 
 msec_t millisSinceStartup();
